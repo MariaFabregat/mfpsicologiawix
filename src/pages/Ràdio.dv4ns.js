eef021072bd32886c907b6e405f354c0487db9d1
Maria <maria.fabregat.giribet@gmail.com>
2025-07-21 00:02:25 +0200

    MFP-001 BASELINE

--- src/pages/Ràdio.dv4ns.js
+++
@@ -1,7 +0,0 @@
-Radio
-// Para obtener la documentación completa sobre las funciones API, incluidos ejemplos para programa con código, visita http://wix.to/94BuAAs
-
-$w.onReady(function () {
-	//TODO: write your page related code here...
-
-});
